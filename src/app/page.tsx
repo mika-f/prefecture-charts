0be9027103c90c93f6d91cfe11fddd0e23f077dc
Natsune Mochizuki <me@natsuneko.cat>
2023-08-14 13:09:09 +0900

    feat: specify title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
+
 import { Layout } from "@/components/Layout";
+
+export const metadata: Metadata = {
+  title: "都道府県別の人口推移グラフ",
+};
 
 export default function Home() {
   return <Layout />;
